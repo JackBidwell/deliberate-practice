78662211b76685ff7061eb8cb751b8589e2cd30e
JackBidwell <jackbidwell2002@gmail.com>
2024-01-29 17:21:49 -0700

    bubbleSort is now a method you can call

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,25 +1,33 @@
+
+def bubbleSort(array)
+
+  p "Your unsorted array is #{array}"
+  i = 0
+  while i < array.length - 1
+    if array[i] > array[i + 1]
+    # if array[i + 1] > array[i]
+      # swap = array[i + 1]
+      # array[i + 1] = array[i]
+      # array[i] = swap
+      # i = 0 
+      swap = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = swap
+      i = 0 
+    else
+    i = i + 1
+    end
+  end
+
+  p "Your sorted array is #{array}"
+end
+
 array = []
+
 i = 0
 while i < 15
   array.push(rand 100)
   i = i + 1
 end
-p "Your unsorted array is #{array}"
-i = 0
-while i < array.length - 1
-  if array[i] > array[i + 1]
-  # if array[i + 1] > array[i]
-    # swap = array[i + 1]
-    # array[i + 1] = array[i]
-    # array[i] = swap
-    # i = 0 
-    swap = array[i]
-    array[i] = array[i + 1]
-    array[i + 1] = swap
-    i = 0 
-  else
-  i = i + 1
-  end
-end
 
-p "Your sorted array is #{array}"
+bubbleSort(array)
